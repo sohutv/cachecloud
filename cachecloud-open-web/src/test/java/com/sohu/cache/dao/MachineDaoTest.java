b65ec92800107cc27d693ba728ffa93bb1dd8fbb
carlosfu <carlosfu@163.com>
2016-03-17 14:20:32 +0800

    fix duplicate machine ip

---
+++ cachecloud-open-web/src/test/java/com/sohu/cache/dao/MachineDaoTest.java
@@ -0,0 +1,41 @@
+package com.sohu.cache.dao;
+
+import com.sohu.cache.entity.MachineInfo;
+import com.sohu.test.BaseTest;
+
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+import java.util.Date;
+
+/**
+ * 机器测试
+ * 
+ * @author leifu
+ * @Date 2016年3月17日
+ * @Time 下午2:15:02
+ */
+public class MachineDaoTest extends BaseTest {
+
+    @Resource
+    private MachineDao machineDao;
+
+    @Test
+    public void testSaveMachine() throws Exception {
+
+        MachineInfo machineInfo = new MachineInfo();
+        machineInfo.setCpu(16);
+        machineInfo.setIp("10.10.53.181");
+        machineInfo.setMem(96);
+        machineInfo.setModifyTime(new Date());
+        machineInfo.setRealIp("");
+        machineInfo.setRoom("北显");
+        machineInfo.setServiceTime(new Date());
+        machineInfo.setSshPasswd("cachecloud-open");
+        machineInfo.setSshUser("cachecloud-open");
+
+        machineDao.saveMachineInfo(machineInfo);
+    }
+
+}
