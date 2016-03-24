eac127c9b61771a09b150f5ab577403adda3e935
zhoufan@dafycredit.com <zhoufan@dafycredit.com>
2016-03-24 16:17:32 +0800

    [添加] 邮件发送组件，需修改配置hostname 并开启disabled； see EmailComponentImpl, see [Commons Email](http://commons.apache.org/proper/commons-email/)

--- cachecloud-open-common/src/main/java/com/sohu/cache/web/component/EmailComponentImpl.java
+++ cachecloud-open-common/src/main/java/com/sohu/cache/web/component/EmailComponentImpl.java
@@ -1,10 +1,16 @@
 package com.sohu.cache.web.component;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 /**
  * 邮件服务
@@ -16,10 +22,13 @@ import java.util.List;
 public class EmailComponentImpl implements EmailComponent {
     private final Logger logger = LoggerFactory.getLogger(EmailComponentImpl.class);
 
-    /**
-     * 管理员
-     */
-    private String adminEmail;
+	/**
+	 * your company send email codes
+	 */
+    private boolean disabled = true;							// 关闭 true | 开启 false
+    private String adminName = "Admin";
+    private String adminEmail = "CacheCloud@dafycredit.com";
+    private String hostName = "cas1.dafycredit.com";
 
     @Override
     public boolean sendMailToAdmin(String title, String content) {
@@ -33,10 +42,47 @@ public class EmailComponentImpl implements EmailComponent {
 
     @Override
     public boolean sendMail(String title, String content, List<String> emailList, List<String> ccList) {
-        /**
-         * your company send email codes
-         */
-        return true;
+    	if(disabled){
+    		return Boolean.FALSE;
+    	}
+    	
+    	if(emailList==null || emailList.size()==0){
+            logger.error("send email fail, nobody.");
+    		return Boolean.FALSE;
+    	}
+
+		try {
+	    	SimpleEmail email = new SimpleEmail();
+			email.setCharset("UTF-8");
+	    	
+			// email.setAuthentication("username", "password");				// 登陆邮件服务器的用户名和密码 （内网，可省略）
+
+    		List <InternetAddress> clist = new ArrayList <InternetAddress>(); 
+    		for(String e : emailList){	
+    			clist.add(new InternetAddress(e)); 							// 接收人
+    		}
+    		email.setTo(clist);
+    		
+//    		List <InternetAddress> cclist = new ArrayList <InternetAddress>(); 
+//    		for(String e : ccList){	
+//    			cclist.add(new InternetAddress(e)); 						// 抄送人
+//    		}
+//    		email.setCc(cclist);
+    		
+			email.setHostName(hostName);									// smtp host
+			email.setFrom(adminEmail, adminName);							// 发送人
+			email.setSubject(title);										// 标题
+			email.setMsg(content);											// 邮件内容
+			email.send();
+			
+			logger.info("Send email successful!");
+		} catch (EmailException | AddressException e) {
+            logger.error("send email error, subject: {}, emailList: {}, ccList: {}", title, emailList, ccList);
+            logger.error(e.getMessage(), e);
+    		return Boolean.FALSE;
+		}
+
+        return Boolean.TRUE;
     }
 
     public void setAdminEmail(String adminEmail) {
