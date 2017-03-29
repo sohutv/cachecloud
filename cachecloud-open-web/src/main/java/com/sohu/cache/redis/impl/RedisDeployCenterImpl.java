467a14c7187bdfbaa31e9abbfff818f09d026d49
carlosfu <carlosfu@163.com>
2017-03-29 15:13:22 +0800

    fix password null

--- cachecloud-open-web/src/main/java/com/sohu/cache/redis/impl/RedisDeployCenterImpl.java
+++ cachecloud-open-web/src/main/java/com/sohu/cache/redis/impl/RedisDeployCenterImpl.java
@@ -399,11 +399,12 @@ public class RedisDeployCenterImpl implements RedisDeployCenter {
         List<String> configs = handleCommonConfig(port, maxMemory);
         if (isCluster) {
             configs.addAll(handleClusterConfig(port));
-        } 
-		//加两个选项
-		configs.add(RedisConfigEnum.REQUIREPASS.getKey() + ConstUtils.SPACE + password);
-		configs.add(RedisConfigEnum.MASTERAUTH.getKey() + ConstUtils.SPACE + password);
-		
+        }
+        if (StringUtils.isNotBlank(password)) {
+	        	//加两个选项
+	    		configs.add(RedisConfigEnum.REQUIREPASS.getKey() + ConstUtils.SPACE + password);
+	    		configs.add(RedisConfigEnum.MASTERAUTH.getKey() + ConstUtils.SPACE + password);
+        }
         printConfig(configs);
         String fileName;
         String runShell;
