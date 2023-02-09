ccf5c3bbed3e6e96bed6c91d279aeca47f934281
ZengyiZhao <gitHub12345678@163.com>
2023-02-09 10:58:44 +0800

    Update update 3.0 to 3.1.sql

--- cachecloud-web/sql/update 3.0 to 3.1.sql
+++ cachecloud-web/sql/update 3.0 to 3.1.sql
@@ -1,2 +1,2 @@
 -- system_resource change
-ALTER TABLE system_resource ADD order_num int(6) NOT NULL DEFAULT '0' COMMENT '≈≈–Ú';
+ALTER TABLE system_resource ADD order_num int(6) NOT NULL DEFAULT '0' COMMENT 'ÊéíÂ∫è';
